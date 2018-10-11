e9fff9bf9545263e7f75164a1e9c83031faa5f37
Jason Mu <jasonmu867@gmail.com>
2018-10-11 19:27:22 +0930

    Added SeedData in Models Folder but not working

---
+++ AFTS/Models/SeedData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace AFTS.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new tennisContext(
+                serviceProvider.GetRequiredService<DbContextOptions<tennisContext>>()))
+            {
+                
+
+                context.Member.AddRange(
+                     new Member
+                     {
+                         MemberId = 1,
+                         Name = "admin",
+                         Nickname = "admin",
+                         Dob = DateTime.Parse("1991-01-01"),
+                         Gender = "M",
+                         Biography = "admin",
+                         Email = "admin@admin.com",
+                         Password = "admin",
+                         RoleId = 1,
+
+                     },
+
+                     new Member
+                     {
+                         MemberId = 2,
+                         Name = "coach",
+                         Nickname = "coach",
+                         Dob = DateTime.Parse("1991-01-01"),
+                         Gender = "M",
+                         Biography = "coach",
+                         Email = "coach@coach.com",
+                         Password = "coach",
+                         RoleId = 2,
+                     }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
